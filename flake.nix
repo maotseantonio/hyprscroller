0c16058a5195f7ebac64b02a28bed19a273772dd
maotseantonio <thetzinantonio@gmail.com>
2025-05-26 20:44:42 +0630

    edit-correct-owner-name

--- flake.nix
+++ flake.nix
@@ -64,7 +64,7 @@
             src =
               if (commitPins ? ${hyprland.rev}) && (self ? rev) then
                 (builtins.fetchGit {
-                  url = "https://github.com/maotseantonio/hyprscroller";
+                  url = "https://github.com/cpiber/hyprscroller";
                   rev = selfRev;
                 })
               else
